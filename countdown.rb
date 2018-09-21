0e6b81f5a8d88be75f968bf572729a8e671ebea8
Chett Tiller <chett.tiller@gmail.com>
2018-09-21 20:43:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)"
+    "#{number} SECOND(S)"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -14,5 +14,5 @@ def countdown_with_sleep(number)
     number -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
